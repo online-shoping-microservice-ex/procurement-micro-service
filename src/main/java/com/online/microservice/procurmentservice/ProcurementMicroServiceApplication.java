fa174d32481323e41a40249ea5b13d2a4329b6ec
kseshasai <kseshasai@SYS-66.mshome.net>
2020-02-18 20:08:31 +0530

    inital setup

---
+++ src/main/java/com/online/microservice/procurmentservice/ProcurementMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.online.microservice.procurmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcurementMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/online/microservice/procurmentservice/ProcurementMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.microservice.procurmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcurementMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
